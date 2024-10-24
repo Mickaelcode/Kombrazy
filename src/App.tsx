f740876747f3082abff23ab947a304d680c9d9da
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2024-10-24 16:55:53 +0300

    feat: Make static value to the props

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <ItemCard />
+      <ItemCard title='My title' subtitle='This is my subtitle' status='Accepted' content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia illum eius dicta. Ab est quasi eius sed consequatur laborum, is' />
     </>
   )
 }
