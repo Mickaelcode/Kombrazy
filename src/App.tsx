c0ddac79531929a1cf62d43e2ed514fc9c527d90
njaina <hei.njaina.2@gmail.com>
2024-10-26 07:11:56 +0300

    chore:edit component call

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import './App.css'
-import ItemCard from './components/ItemCard'
+import "./App.css";
+//import ItemCard from "./components/ItemCard";
+import MainComponent from "./components/Layout/MainComponent";
 
 function App() {
-
   return (
     <>
-      <ItemCard title='My title' subtitle='This is my subtitle' status='Accepted' content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia illum eius dicta. Ab est quasi eius sed consequatur laborum, is' />
+      {/*<ItemCard title='My title' subtitle='This is my subtitle' status='Accepted' content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia illum eius dicta. Ab est quasi eius sed consequatur laborum, is' />
+       */}
+      <MainComponent />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
